273f10afd30bc0f7d253a3f92acb50c0d6cd978f
kouraichiahmed2 <kouraichiahmed2@gmail.com>
2023-02-24 21:35:53 +0100

    usercommit

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private url = 'https://reqres.in/api/users'
+  constructor(private httpClient :HttpClient) { }
+
+  getUsers(page:number){
+     return this.httpClient.get(this.url + '?page=' + page) ;
+  }
+}
